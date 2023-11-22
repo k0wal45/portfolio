f3088dd4b4839dbe7e9571eec91c66f7da6ef6f1
k0wal45 <d.kowal.com@gmail.com>
2023-11-22 16:40:10 +0100

    navbar fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,29 @@ const Navbar = () => {
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </label>
           <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 border-2 border-neutral bg-white rounded-box w-52">
+          <li className='text-xl font-montserrat-bold'>
+            <a href='#header'>
+              Home
+            </a>
+          </li>
+
+          <li className='text-xl font-montserrat-bold'>
+            <a href='#about'>
+              About
+            </a>
+          </li>
+
+          <li className='text-xl font-montserrat-bold'>
+            <a href='#projects'>
+              Projects
+            </a>
+          </li>
+
+          <li className='text-xl font-montserrat-bold'>
+            <a href='#contact'>
+              Contact
+            </a>
+          </li>
 
           </ul>
         </div>
