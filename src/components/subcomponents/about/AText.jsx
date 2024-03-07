da330d0e0abbaf8d9da6732013c940091cfce5fd
k0wal45 <d.kowal.com@gmail.com>
2024-03-07 10:26:30 +0100

    fix

--- src/components/subcomponents/about/AText.jsx
+++ src/components/subcomponents/about/AText.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AText = ({title, header, text}) => {
   return (
-    <div className='flex flex-col items-start justify-center gap-4 '>
+    <div className='flex flex-col items-start justify-center gap-4 max-w-[35rem]'>
       <h2 className="font-bold text-xl font-montserrat-black text-primary bg-primary bg-opacity-30 p-4 w-min whitespace-nowrap">
         {title}
       </h2>
