d707992c7b73261efe11619bba45503dfbc0b1a8
Daniel Kowalski <77574206+k0wal45@users.noreply.github.com>
2024-08-15 12:28:00 +0200

    small fixes

--- src/components/subcomponents/about/AImg.jsx
+++ src/components/subcomponents/about/AImg.jsx
@@ -1,7 +1,11 @@
-const AImg = ({image, alt}) => {
+const AImg = ({ image, alt }) => {
   return (
-    <img src={image} alt={alt} className='rounded-xl flex-1 lg:max-w-[35rem] lg:max-h-[25rem] object-cover'/>
-  )
-}
+    <img
+      src={image}
+      alt={alt}
+      className="rounded-xl flex-1 w-full aspect-square lg:aspect-auto lg:max-w-[35rem] lg:max-h-[25rem] object-cover"
+    />
+  );
+};
 
-export default AImg
+export default AImg;
